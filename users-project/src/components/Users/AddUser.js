1eb8bd44645fa60497145397a30a8acf762c5e67
Miiaroma <miia.romanainen@gmail.com>
2023-02-04 15:05:19 +0200

    Validation and resetting locig added.

--- users-project/src/components/Users/AddUser.js
+++ users-project/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, {state, useState} from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -11,7 +11,18 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    if(enteredUsername.trim().length ===0 || enteredAge.trim().length ===0){
+      return;
+    }
+    
+    if(+enteredAge < 1){
+      return;
+    }
+
     console.log(enteredUsername, enteredAge);
+    setEnterredUsername('');
+    setEnterredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -26,9 +37,9 @@ setEnterredUsername(event.target.value);
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
       </Card>
