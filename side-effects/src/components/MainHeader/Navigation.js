9c48c0010c19b35477d17f0fe69adf932a049793
Miiaroma <miia.romanainen@gmail.com>
2023-02-08 10:46:33 +0200

    Made context dynamic.

--- side-effects/src/components/MainHeader/Navigation.js
+++ side-effects/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import React, { useContext } from 'react';
 import AuthContex from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContex);
 
   return (        
@@ -21,7 +21,7 @@ const Navigation = (props) => {
       )}
       {ctx.isLoggedIn && (
         <li>
-          <button onClick={props.onLogout}>Logout</button>
+          <button onClick={ctx.onLogout}>Logout</button>
         </li>
       )}
     </ul>
