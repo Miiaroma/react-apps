b64c6afe89cc755f27fc0a4f8b9d401533c13404
Miiaroma <miia.romanainen@gmail.com>
2023-02-08 10:26:37 +0200

    Usint the React context.

--- side-effects/src/components/MainHeader/MainHeader.js
+++ side-effects/src/components/MainHeader/MainHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
+import AuthContex from '../../store/auth-context';
 
 const MainHeader = (props) => {
   return (
