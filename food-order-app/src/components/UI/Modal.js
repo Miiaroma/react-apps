d8aa5bf42d0383aeb968c780ea1c29b8406d3a34
Miiaroma <miia.romanainen@gmail.com>
2023-02-09 13:20:43 +0200

    Cart and Modal state modified.

--- food-order-app/src/components/UI/Modal.js
+++ food-order-app/src/components/UI/Modal.js
@@ -1,25 +1,31 @@
-import { Fragment} from 'react';
+import { Fragment } from 'react';
 import ReactDOM  from 'react-dom';
 
 import classes from './Modal.module.css';
 
-const Backdrop = props => {
-return <div className={classes.Backdrop}></div>
+const Backdrop = (props) => {
+return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
-const ModalOverlay = props => {
-return <div className={classes.modal}>
+const ModalOverlay = (props) => {
+return (<div className={classes.modal}>
     <div className={classes.content}>{props.children}</div>
 </div>
+);
 };
 
 const PortalElement = document.getElementById('overlays');
 
-const Modal = props => {
-return <Fragment>
-    {ReactDOM.createPortal(<Backdrop/>, PortalElement)}
-    {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, PortalElement)}
+const Modal = (props) => {
+return (
+<Fragment>
+    {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, PortalElement)}
+    {ReactDOM.createPortal(
+    <ModalOverlay>{props.children}</ModalOverlay>, 
+    PortalElement
+    )}
 </Fragment>
+);
 };
 
 
