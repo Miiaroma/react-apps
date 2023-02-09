17f89ee5ecce169091a6c0f150ffa5f90e199236
Miiaroma <miia.romanainen@gmail.com>
2023-02-09 14:27:14 +0200

    Cart reducer added.

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import { useReducer} from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
 
-const addItemToCarthandler = (item) => {};
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount = state.totalAmount + action.item.price*action.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+      };    
+    }
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+const addItemToCartHandler = (item) => {
+    dispatchCartAction({type: 'ADD', item: item});
+};
 
 const removeItemFromCartHandler = (id) => {};
 
 const cartContext ={
-items: [],
-totalAmount: 0,
-addItem: addItemToCarthandler,
+items: cartState.items,
+totalAmount: cartState.totalAmount,
+addItem: addItemToCartHandler,
 removeItem: removeItemFromCartHandler,
 };
 
