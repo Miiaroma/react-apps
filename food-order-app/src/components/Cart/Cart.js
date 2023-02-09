e2f2cadd248e1a989e318af22ee44e933e32a164
Miiaroma <miia.romanainen@gmail.com>
2023-02-09 11:43:27 +0200

    Modal via React portal added.

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = props => {
@@ -5,15 +6,16 @@ const Cart = props => {
         {id: 'cd1', name: 'Sushi', amount: 2, price: 12.99, }].map((item) => <li>{item.name}</li>)}</ul>;
     
     return (
-    <div>{cartItems}
+    <Modal>{cartItems}
         <div className={classes.total}>
         <span>Total Amount</span>
         <span>36.62</span>
         </div>
-        <div className={classes.actions}></div>
+        <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
-    </div>
+        </div>
+    </Modal>
     );
 };
 
