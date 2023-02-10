871f98954b2a4af2a75d9ace2a5645a0bc1e747d
Miiaroma <miia.romanainen@gmail.com>
2023-02-10 12:56:10 +0200

    Using useEffects for requests.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,9 +6,9 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState (false);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null); 
   
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () =>{
     setIsLoading(true);
     setError(null);
     try{
@@ -33,7 +33,11 @@ function App() {
       setError(error.message);    
     } 
       setIsLoading(false);
-  } 
+  }, []); 
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>
   
